7d2bf5d23dc36e0b3b88c98b97798809a0eefa96
sabiutayo <sabiutayo@yahoo.com>
2020-10-17 15:37:45 +0300

    start

---
+++ src/main/java/sda/backend/server/configuration/AppConfiguration.java
@@ -0,0 +1,30 @@
+package sda.backend.server.configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase/liquibase-changelog.xml");
+        liquibase.setDataSource(getDataSource());
+        return liquibase;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/twitter");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("root");
+        return dataSourceBuilder.build();
+    }
+
+}
