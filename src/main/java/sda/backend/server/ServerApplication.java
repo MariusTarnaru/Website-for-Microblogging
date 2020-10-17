a02800d3f87cebc03ac1278209576f4a58beef28
sabiutayo <sabiutayo@yahoo.com>
2020-10-17 14:00:46 +0300

    start

--- src/main/java/sda/backend/server/ServerApplication.java
+++ src/main/java/sda/backend/server/ServerApplication.java
@@ -2,8 +2,10 @@ package sda.backend.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ServerApplication {
 
     public static void main(String[] args) {
