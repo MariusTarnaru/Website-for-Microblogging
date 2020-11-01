55a6b50a4b1033704806c420ab2f3d87b654d48b
sabiutayo <sabiutayo@yahoo.com>
2020-11-01 10:36:08 +0200

    security work

--- src/main/java/sda/backend/server/configuration/WebConfig.java
+++ src/main/java/sda/backend/server/configuration/WebConfig.java
@@ -3,7 +3,6 @@ package sda.backend.server.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
