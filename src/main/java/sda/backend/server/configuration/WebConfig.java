cec6f93c6593b71a9f12d2890c8554988c56f8b9
sabiutayo <sabiutayo@yahoo.com>
2020-11-01 10:57:41 +0200

    security work

--- src/main/java/sda/backend/server/configuration/WebConfig.java
+++ src/main/java/sda/backend/server/configuration/WebConfig.java
@@ -1,30 +1,16 @@
 package sda.backend.server.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class WebConfig {
-    @Value("${cors.allowedOrigins}")
-    private String allowedOrigins;
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-//    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin(allowedOrigins);
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
